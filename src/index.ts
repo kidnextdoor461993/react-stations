89d496a712de203f8394bcbdfc1a30e790fa0882
Neo <kidnextdoor461993@gmail.com>
2024-05-02 05:17:50 +0700

    [Init] TS config, station class common use, install typescript

---
+++ src/index.ts
@@ -0,0 +1,95 @@
+export type TListener<T> = (arsg: T) => any;
+
+
+/**
+ * Parent station
+ */
+export class Station<TState extends {}> {
+
+    private readonly _listeners = new Map<
+        TListener<TState>,
+        TListener<TState>
+    >();
+
+    protected state: TState = Object();
+
+    /**
+     * Subcribe station by listener(s) 
+     * @param args
+     */
+    public subscribe(
+        args: Required<{
+            listeners: TListener<TState> | Array<
+                TListener<TState>
+            >;
+        }>
+    ) {
+        const { listeners } = args;
+        const convertedListeners = !Array.isArray(listeners) ? [listeners] : listeners;
+
+        try {
+            for (let i = 0; i < convertedListeners.length; i++) {
+                if (this._listeners.has(convertedListeners[i])) {
+                    continue;
+                }
+
+                this._listeners.set(convertedListeners[i], convertedListeners[i]);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    /**
+     * Unsubcribe station on specific listenter(s)
+     * @param args
+     */
+    public unsubscribe(
+        args: Required<{
+            listeners: TListener<TState> | Array<
+                TListener<TState>
+            >;
+        }>
+    ) {
+        const { listeners } = args;
+        const convertedListeners = !Array.isArray(listeners) ? [listeners] : listeners;
+
+        try {
+            for (let i = 0; i < convertedListeners.length; i++) {
+                if (!this._listeners.has(convertedListeners[i])) {
+                    continue;
+                }
+
+                this._listeners.delete(convertedListeners[i]);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    /**
+     * Update state and emit new state changes to listening React component(s)
+     * @param args
+     */
+    protected setState(
+        args: TState
+    ) {
+        Object.assign(this.state, args);
+
+        try {
+            for (const [key] of this._listeners.entries()) {
+                key(this.$state);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    /**
+     * State GETTER
+     */
+    get $state() {
+        return { ...this.state };
+    }
+
+}
